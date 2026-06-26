acfbab2be280717b26c0975071783c432b4aae8b
henriqueVMdev <henrique.de.paula.valim@gmail.com>
2026-06-26 00:55:20 -0300

    loop de funcoes

--- src/EX1.java
+++ src/EX1.java
@@ -10,12 +10,12 @@ public class EX1 {
 * caso o limite de cheque especial seja usado assim que possivel a conta deve cobrar uma taxa de 20% do valor do cheque especial*/
 
     public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String name;
-    double primeiroDeposito;
+        Scanner sc = new Scanner(System.in);
+        String name;
+        double primeiroDeposito;
 
         System.out.println("Bem vindo ao banco SkyBank, por favor digite seu nome: ");
-            name = sc.nextLine();
+        name = sc.nextLine();
 
         System.out.println("Seja muito bem vindo senhor " + name + " sera um prazer lhe atender");
 
@@ -26,46 +26,47 @@ public class EX1 {
         System.out.println("Deposito: " + primeiroDeposito + " confirma este valor? tecle Sim para confirmar e Nao para tentar denovo");
         String escolha = sc.nextLine();
 
-            while (! escolha.equals( "Sim")){
-                System.out.println("Insira a quantia de seu primeiro deposito: ");
-                primeiroDeposito = sc.nextDouble();
-                sc.nextLine();
-                System.out.println("Deposito: " + primeiroDeposito + " confirma este valor? tecle Sim para confirmar e Nao para tentar denovo");
-                escolha = sc.nextLine();
-            }
+        while (!escolha.equals("Sim")) {
+            System.out.println("Insira a quantia de seu primeiro deposito: ");
+            primeiroDeposito = sc.nextDouble();
+            sc.nextLine();
+            System.out.println("Deposito: " + primeiroDeposito + " confirma este valor? tecle Sim para confirmar e Nao para tentar denovo");
+            escolha = sc.nextLine();
+        }
 
-        Conta conta = new Conta( name, primeiroDeposito);
+        Conta conta = new Conta(name, primeiroDeposito);
 
-        System.out.println("conta criada com sucesso!! bem vindo ao nosso banco "+ name);
-
+        System.out.println("conta criada com sucesso!! bem vindo ao nosso banco " + name);
         System.out.println(conta);
-        System.out.print("Precione [1] para sacar \nPrecione [2] para depositar \nPrecione [3] para consultar cheque especial \nPrecione [4] para pagar boleto \n");
+
+        System.out.print("Precione [1] para sacar \nPrecione [2] para depositar \nPrecione [3] para consultar cheque especial " +
+                "\nPrecione [4] para pagar boleto \nPrecione[9] para sair ");
         int escolhaAcao = sc.nextInt();
 
-        if (escolhaAcao == 1){
-                System.out.println("Insira a quantidade que deseja sacar");
+        while (escolhaAcao != 9) {
+
+            if (escolhaAcao == 1) {
+                System.out.println("Insira a quantidade que deseja sacar ");
                 double saque = sc.nextDouble();
                 conta.sacar(saque);
-        }
-        else if ( escolhaAcao == 2){
-            System.out.println("Insira a quantidade que deseja depositar");
-            double deposito = sc.nextDouble();
-            conta.depositar(deposito);
-        }
-
-        else if ( escolhaAcao == 3){
-            if (conta.usandoCheque()){
-                System.out.println("Saldo negativo, conta usando cheue especial");
+            } else if (escolhaAcao == 2) {
+                System.out.println("Insira a quantidade que deseja depositar ");
+                double deposito = sc.nextDouble();
+                conta.depositar(deposito);
+            } else if (escolhaAcao == 3) {
+                if (conta.usandoCheque()) {
+                    System.out.println("Saldo negativo, conta usando cheue especial ");
+                } else {
+                    System.out.println("Nao esta usando cheque especial, saldo positivo tudo ok.");
+                }
+            } else if (escolhaAcao == 4) {
+                System.out.println("Insira o valor do boleto ");
+                double valorBoleto = sc.nextDouble();
+                conta.pagarBoleto(valorBoleto);
             }
-            else{
-                System.out.println("Nao esta usando cheque especial, saldo positivo tudo ok,");
-            }
-        }
-
-        else if ( escolhaAcao == 4){
-            System.out.println("Insira o valor do boleto");
-            double valorBoleto = sc.nextDouble();
-            conta.pagarBoleto(valorBoleto);
+            System.out.print("Precione [1] para sacar \nPrecione [2] para depositar \nPrecione [3] para consultar cheque especial " +
+                    "\nPrecione [4] para pagar boleto \nPrecione[9] para sair ");
+            escolhaAcao = sc.nextInt();
         }
     }
-}
+    }
